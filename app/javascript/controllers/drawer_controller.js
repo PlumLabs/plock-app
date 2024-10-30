868aafca0bbb91ccb13c67a309bf96ccbc968199
Edu Depetris <edu.depetris.00@gmail.com>
2024-10-30 09:47:24 +0100

    Add drawer sidebar

---
+++ app/javascript/controllers/drawer_controller.js
@@ -0,0 +1,29 @@
+import { Controller } from "@hotwired/stimulus"
+import { enter, leave } from "transition"
+
+// Connects to data-controller="drawer"
+export default class extends Controller {
+  static targets = [ "panel", "backdrop", "closeButton", "container" ]
+
+  close() {
+    console.log("Drawer close")
+
+    Promise.all([
+      leave(this.panelTarget),
+      leave(this.backdropTarget),
+      leave(this.closeButtonTarget),
+    ]).then(() => {
+      this.containerTarget.classList.add("hidden")
+    }).catch(() => {
+      // Fallback in case the animation fails
+      this.containerTarget.classList.add("hidden")
+    })
+  }
+
+  open() {
+    this.containerTarget.classList.remove("hidden");
+    enter(this.panelTarget)
+    enter(this.backdropTarget)
+    enter(this.closeButtonTarget)
+  }
+}
