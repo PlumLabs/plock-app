2d23ce518c23821df2518bd85f02509be4a11817
Edu Depetris <edu.depetris.00@gmail.com>
2024-11-04 09:27:33 +0100

    Create and destroy users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :ensure_can_administrate
-  before_action :set_user, only: %i[ create ]
+  before_action :set_user, only: %i[ destroy ]
 
   def index
     @users = User.all
@@ -15,10 +15,18 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: "User was successfully created." }
+        format.html { redirect_to users_path, notice: "created" }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
+    end
+  end
+
+  def destroy
+    @user.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to users_path, notice: "destroyed" }
     end
   end
 
@@ -28,6 +36,6 @@ class UsersController < ApplicationController
     end
 
     def user_params
-      params.expect(user: [ :first_name, :last_name, :email_address, :password, :password_confirmation ])
+      params.expect(user: [ :first_name, :last_name, :email_address, :password, :role ])
     end
 end
