bfafcbd5e2ca8e9ff82b022610f10f0288e72b13
Edu Depetris <edu.depetris.00@gmail.com>
2024-11-06 18:48:57 +0100

    Add sucess alert after actions

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -21,11 +21,9 @@ class ClientsController < ApplicationController
 
     respond_to do |format|
       if @client.save
-        format.html { redirect_to @client, notice: "Client was successfully created." }
-        format.json { render :show, status: :created, location: @client }
+        format.html { redirect_to @client, notice: "created" }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @client.errors, status: :unprocessable_entity }
       end
     end
   end
