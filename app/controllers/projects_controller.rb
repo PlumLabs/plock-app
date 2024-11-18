d7d63c4b1fd2290fd34d43c809e90ad61944839c
Edu Depetris <edu.depetris.00@gmail.com>
2024-11-18 19:11:33 +0100

    Clean up views

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -22,10 +22,8 @@ class ProjectsController < ApplicationController
     respond_to do |format|
       if @project.save
         format.html { redirect_to @project, notice: "Project was successfully created." }
-        format.json { render :show, status: :created, location: @project }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
       end
     end
   end
