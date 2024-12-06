3f084dd9d4a67089da185d17e8dfe4e6217f53fb
Edu Depetris <edu.depetris.00@gmail.com>
2024-12-06 12:43:52 +0100

    Fix system test issue with windows sizes

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,9 +1,10 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  WINDOW_SIZE = [ 1400, 1400 ]
   include SystemTestHelper
 
   Capybara.enable_aria_label = true
 
-  driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+  driven_by :selenium, using: :headless_chrome, screen_size: WINDOW_SIZE
 end
--- test/mobile_system_test_case.rb
+++ test/mobile_system_test_case.rb
@@ -1,5 +1,20 @@
-require "application_system_test_case"
+require "test_helper"
 
-class MobileSystemTestCase < ApplicationSystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [ 375, 667 ]
+class MobileSystemTestCase < ActionDispatch::SystemTestCase
+  WINDOW_SIZE = [ 375, 667 ]
+  include SystemTestHelper
+
+  Capybara.enable_aria_label = true
+
+  driven_by :selenium, using: :headless_chrome, screen_size: WINDOW_SIZE
+
+  setup do
+    # force the mobile window size before each test
+    current_window.resize_to(*WINDOW_SIZE)
+  end
+
+  teardown do
+    # go back to the default window size to avoid side effects
+    current_window.resize_to(*ApplicationSystemTestCase::WINDOW_SIZE)
+  end
 end
