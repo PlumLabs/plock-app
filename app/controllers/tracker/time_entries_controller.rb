c9cb4fb216ebb0f7f824f7fbda628940aa6f9612
Edu Depetris <edu.depetris.00@gmail.com>
2024-12-12 11:19:38 +0100

    Add turbo morph and enhance ux

--- app/controllers/tracker/time_entries_controller.rb
+++ app/controllers/tracker/time_entries_controller.rb
@@ -9,7 +9,10 @@ class Tracker::TimeEntriesController < ApplicationController
       if @time_entry.save
         format.html { redirect_to tracker_path, notice: "created" }
       else
-        format.html { render partial: "form", locals: { time_entry: @time_entry }, status: :unprocessable_entity }
+        format.html { render tracker_path, notice: "not_created" }
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace(@time_entry, partial: "form", locals: { time_entry: @time_entry })
+        end
       end
     end
   end
@@ -17,10 +20,12 @@ class Tracker::TimeEntriesController < ApplicationController
   def update
     respond_to do |format|
       if @time_entry.update(time_entry_params)
-        # format.html { render partial: "form", locals: { time_entry: @time_entry, updated: true } }
         format.html { redirect_to tracker_path, notice: "updated" }
       else
-        format.html { render partial: "form", locals: { time_entry: @time_entry }, status: :unprocessable_entity }
+        format.html { render tracker_path, notice: "not_updated" }
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace(@time_entry, partial: "form", locals: { time_entry: @time_entry }, method: :morph)
+        end
       end
     end
   end
