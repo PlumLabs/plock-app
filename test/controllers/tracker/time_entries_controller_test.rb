de033b804fd22d9ccf60ada989650b33714b8651
Edu Depetris <edu.depetris.00@gmail.com>
2025-01-03 14:54:37 +0100

    Allow project manager to update entries from other users

--- test/controllers/tracker/time_entries_controller_test.rb
+++ test/controllers/tracker/time_entries_controller_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 class Tracker::TimeEntriesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    projects(:two).project_assignments.create!(user: users(:franco), role: :manager)
+    projects(:two).project_assignments.create!(user: users(:edu), role: :member)
+  end
+
   test "logged out users cannot manipulate time entries" do
     post tracker_time_entries_url, params: {
       time_entry: { date: "2021-05-01", description: "Worked on the project", duration: "03:34", user_id: users(:franco).id }
@@ -24,7 +29,7 @@ class Tracker::TimeEntriesControllerTest < ActionDispatch::IntegrationTest
     end
   end
 
-  test "member cannot create/update a time entry for another user" do
+  test "users cannot create/update a time entry for another user" do
     sign_in users(:franco)
 
     assert_no_difference("TimeEntry.count") do
@@ -34,6 +39,19 @@ class Tracker::TimeEntriesControllerTest < ActionDispatch::IntegrationTest
     end
   end
 
+  test "project manager can update a time entry for another user in the same project" do
+    time_entry = TimeEntry.create!(date: "2021-05-01", description: "test", duration: "03:34", user: users(:edu), project: projects(:two))
+
+    sign_in users(:franco)
+
+    patch tracker_time_entry_url(time_entry), params: {
+      time_entry: { date: "2022-02-02", description: "test", duration: "03:34", user_id: users(:edu).id, project_id: projects(:two).id }
+    }
+
+    time_entry.reload
+    assert_equal time_entry.date, "2022-02-02".to_date
+  end
+
   test "admin can destroy a time entry for another users" do
     time_entry = TimeEntry.create!(date: "2021-05-01", description: "Worked on the project", duration: "03:34", user: users(:franco))
 
@@ -41,6 +59,21 @@ class Tracker::TimeEntriesControllerTest < ActionDispatch::IntegrationTest
 
     assert_difference("TimeEntry.count", -1) do
       delete tracker_time_entry_url(time_entry)
+    end
+  end
+
+  test "project manager can destroy a time entry for another user in the same project" do
+    no_project_time_entry = TimeEntry.create!(date: "2021-05-01", description: "test", duration: "03:34", user: users(:edu))
+    project_time_entry = TimeEntry.create!(date: "2021-05-01", description: "test", duration: "03:34", user: users(:edu), project: projects(:two))
+
+    sign_in users(:franco)
+
+    assert_difference("TimeEntry.count", -1) do
+      delete tracker_time_entry_url(project_time_entry)
+    end
+
+    assert_no_difference("TimeEntry.count") do
+      delete tracker_time_entry_url(no_project_time_entry)
     end
   end
 
@@ -59,7 +92,9 @@ class Tracker::TimeEntriesControllerTest < ActionDispatch::IntegrationTest
 
     sign_in users(:franco)
 
-    assert_difference("TimeEntry.count", -1) do
+    assert TimeEntry.count, 1
+
+    assert_difference("TimeEntry.count", 0) do
       delete tracker_time_entry_url(time_entry)
     end
   end
