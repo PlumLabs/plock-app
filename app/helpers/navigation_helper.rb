b71937d0f5ba2d7e073e175e3f1f19724908c0e4
Edu Depetris <edu.depetris.00@gmail.com>
2025-01-03 19:00:42 +0100

    Add fuzzy matching for nav

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,7 +1,7 @@
 module NavigationHelper
   def nav_link_to(name, path, options = {}, &block)
     classes = options[:class] || ""
-    active = current_page?(path)
+    active = current_page?(path) || request.path.start_with?(path)
     active_classes = "bg-gray-50 text-indigo-600"
     default_classes = "text-gray-700 hover:text-indigo-600 hover:bg-gray-50"
 
