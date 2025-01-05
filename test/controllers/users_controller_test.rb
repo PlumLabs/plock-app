6f4fdf87ec15751d40fecb99eaa9c0d02ad02fe4
Edu Depetris <edu.depetris.00@gmail.com>
2025-01-05 18:56:53 +0100

    Add test for pagination links

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -60,4 +60,32 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_no_match /Franco/, @response.body
     assert_match /Eduardo/, @response.body
   end
+
+  test "users are paginated" do
+    10.times do
+      first_name = Faker::Name.first_name
+      last_name = Faker::Name.last_name
+      email = Faker::Internet.unique.email(name: first_name, domain: "plum.com.ar")
+      User.create!(email_address: email, first_name: first_name, last_name: last_name, password: "123456")
+    end
+
+    sign_in users(:edu)
+
+    get users_url, params: { per_page: 5, keep_me: { pagination: "should-kep-this" } }
+    assert_select "a[href=?]", users_path(per_page: 5, page: 2, keep_me: { pagination: "should-kep-this" }), text: /Next/
+    assert_select "a", { text: /Previous/, count: 0 }
+
+    # Navigate to the second page to test the presence of the "Previous" link
+    get users_url, params: { page: 2, per_page: 5, keep_me: { pagination: "should-kep-this" } }
+    assert_select "a", { text: /Next/ }
+    assert_select "a[href=?]", users_path(per_page: 5, page: 1, keep_me: { pagination: "should-kep-this" }), text: /Previous/
+  end
+
+  test "does not show pagination when no enough records" do
+    sign_in users(:edu)
+
+    get users_url
+    assert_select "a", { text: /Previous/, count: 0 }
+    assert_select "a", { text: /Next/, count: 0 }
+  end
 end
