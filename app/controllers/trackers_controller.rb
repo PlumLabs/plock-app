b7230355ce4164b8c8d30a2ca1d1df9639c5cb05
Edu Depetris <edu.depetris.00@gmail.com>
2025-01-06 10:42:40 +0100

    Add pagination to tracker (#47)

--- app/controllers/trackers_controller.rb
+++ app/controllers/trackers_controller.rb
@@ -1,7 +1,7 @@
 class TrackersController < ApplicationController
   def show
     @time_entry = Current.user.time_entries.new(date: Date.current)
-    @time_entries_grouped_by_week = Current.user.time_entries.order(date: :desc).group_by do |entry|
+    @time_entries_grouped_by_week = Current.user.time_entries.order(date: :desc).then(&paginate).group_by do |entry|
       entry.date.beginning_of_week
     end
   end
