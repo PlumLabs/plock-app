91a39783232c9534f279d78101b0693ccaf7c247
Edu Depetris <edu.depetris.00@gmail.com>
2025-01-06 10:39:37 +0100

    Add pagination to tracker

--- test/controllers/trackers_controller_test.rb
+++ test/controllers/trackers_controller_test.rb
@@ -12,4 +12,34 @@ class TrackerControllerTest < ActionDispatch::IntegrationTest
     get tracker_url
     assert_response :redirect
   end
+
+  test "time entries are paginated" do
+    10.times do
+      TimeEntry.create!(
+        user: users(:edu),
+        duration_minutes: rand(120..240),
+        description: "test",
+        date: Faker::Date.between(from: 3.months.ago, to: Date.today)
+      )
+    end
+
+    sign_in users(:edu)
+
+    get tracker_url, params: { per_page: 3 }
+    assert_select "a", { text: /Next/ }
+    assert_select "a", { text: /Previous/, count: 0 }
+
+    # Navigate to the second page to test the presence of the "Previous" link
+    get tracker_url, params: { page: 2, per_page: 3 }
+    assert_select "a", { text: /Next/ }
+    assert_select "a", { text: /Previous/ }
+  end
+
+  test "does not show pagination when no enough records" do
+    sign_in users(:edu)
+
+    get tracker_url
+    assert_select "a", { text: /Previous/, count: 0 }
+    assert_select "a", { text: /Next/, count: 0 }
+  end
 end
