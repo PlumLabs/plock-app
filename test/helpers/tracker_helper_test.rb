bfa89ec361ddeab8b7a45ed246ed8097781d2ab1
Edu Depetris <edu.depetris.00@gmail.com>
2025-01-20 13:34:36 +0100

    Group the entries by week and day on Tracker (#54)
    * Group the entries by week and day on Tracker
    
    * ui tweaks

--- test/helpers/tracker_helper_test.rb
+++ test/helpers/tracker_helper_test.rb
@@ -5,4 +5,10 @@ class TrackerHelperTest < ActionView::TestCase
     assert_equal "1h 30m", minutes_to_hours(90)
     assert_equal "0h 0m", minutes_to_hours(0)
   end
+
+  test "#human_date_format returns a human version of a date" do
+    assert_equal "Today", human_date_format(Date.today)
+    assert_equal "Yesterday", human_date_format(Date.yesterday)
+    assert_equal "January 01, 2021", human_date_format(Date.new(2021, 1, 1))
+  end
 end
