d34c59d798247f8dd7829841200d41a6285db395
Edu Depetris <edu.depetris.00@gmail.com>
2025-05-12 10:48:53 +0200

    Try to avoid Change your passwrod popup

--- test/mobile_system_test_case.rb
+++ test/mobile_system_test_case.rb
@@ -6,7 +6,9 @@ class MobileSystemTestCase < ActionDispatch::SystemTestCase
 
   Capybara.enable_aria_label = true
 
-  driven_by :selenium, using: :headless_chrome, screen_size: WINDOW_SIZE
+  driven_by :selenium, using: :headless_chrome, screen_size: WINDOW_SIZE do |options|
+    options.add_argument("--guest")
+  end
 
   # https://github.com/rails/rails/blob/31c060c38225d36a73b5d4787cccc0cb7e1c944a/actionpack/lib/action_dispatch/system_test_case.rb#L69
   # As screen_size is ignored on headless_chrome with selenium, we need to resize the window manually
